ea657a2263044b3fe8dffd0b4fa4a693673a625b
rezainiet <masudrezaog@gmail.com>
2022-06-21 16:48:33 +0600

    trying to solve cors policy

--- index.js
+++ index.js
@@ -26,14 +26,23 @@ const corsConfig = {
 //     );
 //     next();
 // });
-app.use(cors());
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+const corsOpts = {
+    origin: '*',
+
+    methods: [
+        'GET',
+        'POST',
+        'PUT',
+        'PATCH',
+        'DELETE'
+    ],
+
+    allowedHeaders: [
+        'Content-Type',
+    ],
+};
+
+app.use(cors(corsOpts));
 
 
 
